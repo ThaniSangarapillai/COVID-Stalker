ad4ce4c1e4802041804bfa2dd13bd48b95ab4f43
Thani <62773552+ThaniSangarapillai@users.noreply.github.com>
2020-03-28 10:34:00 -0400

    stuff

--- server/dist/Index.js
+++ server/dist/Index.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const App_1 = __importDefault(require("./App"));
+const s2 = require('@radarlabs/s2');
 const port = 3000;
 App_1.default.listen(port, err => {
     if (err) {
         return console.error(err);
     }
+    const cell1 = new s2.CellId(9926595695177891840n);
+    console.log(cell1.token());
     return console.log(`server is listening on ${port}`);
 });
 //# sourceMappingURL=Index.js.map
